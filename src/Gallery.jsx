97d86de5e7c8d02879c7a373024897799e21d332
Pawel Kozicki <pe.kozicki@gmail.com>
2017-11-03 19:46:25 +0100

    Add audio preview

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,16 +2,53 @@ import React, { Component } from 'react';
 import './Gallery.css';
 
 class Gallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playingUrl: '',
+            audio: null,
+            playing: false
+        }
+    }
+    playAudio(previewUrl) {
+        let audio = new Audio(previewUrl);
+        if (!this.state.playing) {
+            audio.play();
+            this.setState({
+                playing: true,
+                playingUrl: previewUrl,
+                audio
+            })
+        } else {
+            if (this.state.playingUrl === previewUrl) {
+                this.state.audio.pause();
+                this.setState({
+                    playing: false
+                })
+            } else {
+                this.state.audio.pause();
+                audio.play();
+                this.setState({
+                    playing: true,
+                    playingUrl: previewUrl,
+                    audio
+                })
+            }
+        }
+    }
+
     render() {        
         const { tracks } = this.props;
         return (
             <div className="Gallery-wrapper">
                 {tracks.map((track, k) => {
+                    console.log("Track properties: ", track);
                     const trackImg = track.album.images[0].url;
                     return (
                         <div
                             key={ k }
                             className="Track"
+                            onClick={ () => this.playAudio(track.preview_url) }
                         >
                             <img
                                 src={ trackImg }
