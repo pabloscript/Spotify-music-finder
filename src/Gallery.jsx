f96bccbc52b82cd4546ecb5fe02e553bb75702de
Pawel Kozicki <pe.kozicki@gmail.com>
2017-11-03 10:14:37 +0100

    Create a gallery component

---
+++ src/Gallery.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Gallery extends Component {
+    render() {
+        return (
+            <div className="Gallery">
+                Gallery
+            </div>
+        )
+    }
+}
+
+export default Gallery;
