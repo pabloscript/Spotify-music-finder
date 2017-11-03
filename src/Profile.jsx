d26b4fc68463c63e6df46e3b5241d1599c0af10b
Pawel Kozicki <pe.kozicki@gmail.com>
2017-11-03 10:09:57 +0100

    Create an artist profile component

---
+++ src/Profile.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Profile extends Component {
+    render() {
+        let artist = {
+            name: '', 
+            followers: {total: ''}, 
+            images: [{url: ''}],
+            genres: []
+        };
+        artist = this.props.artist !== null ? this.props.artist : artist;
+
+        return (
+            <div className="Profile">
+                <img 
+                    alt="Profile"
+                    className="Profile-img"
+                    src={artist.images[0].url}
+                />
+                <div className="Artist-info">
+                    <div>
+                        {artist.name}
+                    </div>
+                    <div>
+                        {artist.followers.total} followers
+                    </div>
+                    <div>
+                        {
+                            artist.genres.map((genre, k) => {
+                                genre = genre !== artist.genres[artist.genres.length - 1] 
+                                    ? ` ${genre}, ` 
+                                    : `${genre}`;
+                                return (
+                                    <span key={k}>{genre}</span>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Profile;
