78658252c65522ab3b10d774ec5d492a69244e76
Pawel Kozicki <pe.kozicki@gmail.com>
2017-11-03 17:28:57 +0100

    Structure the Gallery component

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,9 +3,29 @@ import './App.css';
 
 class Gallery extends Component {
     render() {
+        console.log(this.props);
+        const { tracks } = this.props;
         return (
-            <div className="Gallery">
-                Gallery
+            <div>
+                {tracks.map((track, k) => {
+                    const trackImg = track.album.images[0].url;
+                    return (
+                        <div
+                            key={ k }
+                            className="Track"
+                        >
+                            <img
+                                src={ trackImg }
+                                className="Track-img"
+                                alt="track"
+                            />
+                            <p className="Track-text">
+                                { track.name }
+                            </p>
+                        </div>
+                        )
+                    })
+                }
             </div>
         )
     }
