b734c64655a4895c0fe219176e84bae9806331cc
Pawel Kozicki <pe.kozicki@gmail.com>
2017-11-03 10:15:40 +0100

    Extend fetch request with top tracks search, include the Gallery component

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import Profile from './Profile';
+import Gallery from './Gallery';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      artist: null,
+      tracks: []
     }
   }
 
   search() {
     const BASE_URL = 'https://api.spotify.com/v1/search?';
-    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    const accessToken = 'BQBIRk0pRGOub4KcVbRTUsc4bVYOrrb-RDZHuk8TSPvvnIJtX_gvj5KvohtwOOb7YFJxh9z1d1Avce27OFSTGVhKBDiSfDvzRZyhId7dLBqZ6xTUTG_jA4T2GwVRwrqlBx93PX7fIWN6__XHw-myLhWI67GjRlDk&refresh_token=AQDGbHgSdWGGbzlLwXnOwOKGwvM5dogbvxluQC24rEjQvx2m7Vzf_ENKkg5z-POm4ZiC5b10imFaJU0P953PtL7F3HhDWmrrgmSAZbmgRG2z-d9lFwyx3vDXxlC8wXES6F0';
+    let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    const accessToken = 'BQDU7dQwV57uqE4N8pSfnMWPFPhcxaoaW8mQ_0iD6Q0U4QgI4e2yBNFs9MqEr4qUhQ4sfTHnt8Kh2NpfH_QlFtT2HAcs3ZndgUhNgbi75L0OVOG7FFRI21MErKWLlHZbW8sVPEQenPvL3KfayOtyo3T-JhGaJAfg&refresh_token=AQAvaZaDsAGTOe4tFcX4pVmOtEzrstgfJHvGbUF2MwFFwnZvjVuN9or1IuXtgLxJLg-KMK-7OxtcPKuyOb4mJy2zEt8NO5pRym36vXpNIWRDIo4HaTsUOh0uc37VFH8PTbI';
     console.log(FETCH_URL);
+    const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
 
-    const myOptions = {
+    const fetchArtist = {
       method: 'GET',
       headers: {
         'Authorization': 'Bearer ' + accessToken
@@ -26,11 +30,27 @@ class App extends Component {
       cache: 'default'
     };
 
-    fetch(FETCH_URL, myOptions)
+    const fetchTopTracks = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      mode: 'cors',
+      cache: 'default'
+    }
+
+    fetch(FETCH_URL, fetchArtist)
       .then(response => response.json())
       .then(json => {
         const artist = json.artists.items[0];
         this.setState({artist});
+
+        FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=PL&`
+        fetch(FETCH_URL, fetchTopTracks)
+          .then(response => response.json())
+          .then(json => {console.log(json)})
+          const { tracks } = json;
+          this.setState({tracks});
       })
   }
 
@@ -56,10 +76,16 @@ class App extends Component {
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <Profile artist={this.state.artist} />
-        <div className="Gallery">
-          Gallery
-        </div>
+        {
+          this.state.artist !== null
+          ?
+            <div>
+              <Profile artist={this.state.artist} />
+              <Gallery tracks={this.state.tracks} />
+            </div>          
+          :
+            <div></div>
+        }        
       </div>
     );
   }
